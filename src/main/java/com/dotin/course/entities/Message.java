ca1a1be36e92b72f3982ef00a93ec6ba1d0cf81f
Saeed Zarinfam <zarinfam.s@gmail.com>
2018-10-17 08:51:59 +0330

    improve test for message service.

--- src/main/java/com/dotin/course/entities/Message.java
+++ src/main/java/com/dotin/course/entities/Message.java
@@ -1,7 +1,9 @@
 package com.dotin.course.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,8 @@ import javax.persistence.Id;
  */
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 public class Message {
